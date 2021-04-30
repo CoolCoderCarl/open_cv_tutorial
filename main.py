cbba02310f266639dbe61b197a257e50f2f22f9b
CoolCoderCarl <123qwekir2wl@gmail.com>
2021-04-30 12:26:29 +0300

    Stage (#6)
    * add sample nn (#3)
    
    * add sample nn
    
    * add a lot of comments; add print for results
    
    * move sigmoid func higher value to 3; update input and prediction results
    
    * remove useless comments
    
    * Update .gitignore; remove neural network forever & add comp vision li… (#5)
    
    * Update .gitignore; remove neural network forever & add comp vision libs and code
    
    * Realize Harris Corner Detector method
    
    * separate code to two funcs; add another which make pic lighter; add main point
    
    * Add comments

--- main.py
+++ main.py
@@ -1,35 +1,50 @@
-from numpy import exp, array, random, dot
+import numpy as np
+import cv2 as cv
+### U CAN PUT HERE EVERY PICTURE U WANT
+filename = 'house.jpg'
 
-### LEARNING DATA FOR NEURON
-training_set_inputs = array([[0, 1, 1], [3, 1, 2], [3, 2, 1], [0, 3, 1], [2, 3, 2]])
-print("INPUT MATRIX")
-print(training_set_inputs)
-### MATRIX TRANSPOSING
-### IF U WANT TO DELETE .T THEN SET (3, 4) IN synaptic_weights VARIABLE
-training_set_outputs = array([[0, 3, 3, 0, 2]]).T
-print("OUTPUT RESULTS")
-print(training_set_outputs)
-random.seed(1)
-### IF U WANT TO MAKE SOME CHANGES TO RESULT U NEED TO CHANGE FIRST AND LAST DIGITS
-### IF U CHANGE (3, 1) TO (3, 4) IT SHOW MATRIX WITH 4 DIFFIRENT DIGITS
-synaptic_weights = 2 * random.random((3, 1)) - 1
-print("SYNAPTIC WEIGHTS AT START")
-print(synaptic_weights)
+###
+### THAT FUNC IS HIGHLIGHT THE CORNER ON PICTURE
+###
+def corner_detector():
+    ### CREATE CONTAINER WITH READED IMAGE
+    img = cv.imread(filename)
+    ### ALWAYS MAKE ORIGINAL IMAGE GRAY
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-### RESULT BECOME MORE ACCURATE IF U INCREASE THE NUMBERS OF THE ROUNDS
-for iteration in range(100000):
-    ### EXP = EXPONENTIATION
-    ### DOT = MATRIX MULTIPLICATION
-    output = 3 / (1 + exp(-(dot(training_set_inputs, synaptic_weights))))
-    ### REWRITE synaptic_weights BY MULTIPLICATION TRANSPARENT INPUTS AND OUTPUTS
-    ### FORMULA CALLED "Weighted by mistake" AND CONTAINS SECOND PART STARTED AFTER COMMA
-    ### SECOND PART OF FORMULA CALLED "Sigmode Gradient"
-    synaptic_weights += dot(training_set_inputs.T, (training_set_outputs - output) * output * (1 - output))
-    ### IF UNCOMMENT STRINGS BELOW U WILL SEE ALL IN-BETWEEN RESULTS. THINK TWICE
-    #print("NEW SYNAPTIC WEIGHTS")
-    #print(synaptic_weights)
+    gray = np.float32(gray)
 
-print("RESULT")
-### HERE WE DIVIDE 3 (1, in original) ON 1 PLUS EXPONENTIONAL MULTIPLICATION OF MATRIX WHICH WE WANT TO PREDICT
-### AND synaptic_weights WHICH U CAN FIND ABOVE
-print(3 / (1 + exp(-(dot(array([3, 0, 0]), synaptic_weights)))))
+    dst = cv.cornerHarris(gray, 2, 3, 0.04)
+    dst = cv.dilate(dst, None)
+
+    img[dst > 0.01 * dst.max()] = [0, 0, 255]
+    cv.imshow('Corner detector', img)
+
+    ### WAIT FOR ANY KEY
+    if cv.waitKey(0) & 0xff == 27:
+        cv.destroyAllWindows()
+
+###
+### THAT FUNC IS MAKE PIC LIGHTER
+###
+def gray_light():
+    img = cv.imread(filename)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    ### ADD LIGHT TO PIXELS
+    gray_add = cv.add(gray, 200)
+    ### MULTIPLY LIGHT TO PIXELS
+    gray_mul = cv.multiply(gray, 1.8)
+
+    cv.imshow('Original gray', img)
+    cv.imshow('Add light', gray_add)
+    cv.imshow('Multuply light', gray_mul)
+
+    ### WAIT FOR ANY KEY
+    if cv.waitKey(0) & 0xff == 27:
+        cv.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    # gray_light()
+    corner_detector()
