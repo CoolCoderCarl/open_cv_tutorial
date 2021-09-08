a3d14b7bd97bc32bac6a2ee0ff6dff17a054ccc2
CoolCoderCarl <123qwekir2wl@gmail.com>
2021-09-08 16:08:43 +0300

    Move numpy; update requirements.txt according GitHub suggestion (#15)

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import computer_vision_l.c_v_l as my_cv
-# import neural_network_l.n_n_l as my_nn
 
 ### You can put here any picture you want
 # filename = 'house.jpg'
@@ -7,9 +6,7 @@ filename = 'test.jpg'
 
 if __name__ == '__main__':
     my_cv.corner_detector(filename)
-    # my_cv.gray_light(filename)
+    my_cv.gray_light(filename)
     my_cv.sift(filename)
 
-    # my_nn.basic()
-    # my_nn.dimensional()
 
